8c9b7383474d0e1786b541f0b71c2aedb31b2dcb
saikrupaelate <elatesaikrupa@gmail.com>
2026-02-21 23:20:24 -0500

    Optimize GitHub Actions workflow and configure remote state

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-storage-elate" # REPLACE with your bucket name
+    key            = "terraform-iam-ec2/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table" # Optional: for state locking
+  }
+}
+
 provider "aws" {
   region = var.aws_region
   # Use AWS credentials provided by environment variables, shared config, or
